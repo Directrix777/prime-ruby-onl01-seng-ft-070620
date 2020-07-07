3b566a30298d377ca3c0c2018675dfd60c0008a5
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-07 19:53:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,11 @@ require 'pry'
 def prime?(num)
   range = (2..num).to_a
   range.each do |divi|
-    if num % divi == 0 && num != divi
+    if num % divi == 0 && num < divi
       return false
     end
+    range.delete_if{|multiple| multiple % divi == 0 && multiple != divi}
+    binding.pry
   end
   num > 1
 end
